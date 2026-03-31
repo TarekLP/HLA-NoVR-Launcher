165303a5d4807f27dfd7c00f8970d0d625a188ca
TarekInnit <76664991+TarekLP@users.noreply.github.com>
2026-03-31 16:06:50 +0200

    Proper Project Creation after everything was lost

---
+++ HLA_NoVRLauncher_Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace HLA_NoVRLauncher_Avalonia.ViewModels
+{
+	public partial class MainWindowViewModel : ViewModelBase
+	{
+		public string Greeting { get; } = "Welcome to Avalonia!";
+	}
+}
---
+++ HLA_NoVRLauncher_Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace HLA_NoVRLauncher_Avalonia.Views
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
